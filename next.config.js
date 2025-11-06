cdadff65fbcdee690a1cde4de4935323f291d5b5
JDennisovich <justin.dennis.sauquillo14@gmail.com>
2025-11-06 22:40:30 +0800

    adjusted next.config.js

--- next.config.js
+++ next.config.js
@@ -5,12 +5,4 @@ module.exports = {
     ignoreDuringBuilds: true, // ✅ Allow build even if ESLint finds errors
   },
 
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8080/:path*', // Replace with your backend API URL
-      },
-    ];
-  },
 };
